e6ce6a5bc1b8de3706ed9f07240ac20a602fd0db
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-09-30 22:00:54 +0100

    swagger and address book relationship async

--- src/main/java/com/auction/users_service/UsersServiceApplication.java
+++ src/main/java/com/auction/users_service/UsersServiceApplication.java
@@ -2,8 +2,10 @@ package com.auction.users_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.auction.users_service")
 public class UsersServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/auction/users_service/config/RestClientConfig.java
@@ -0,0 +1,36 @@
+package com.auction.users_service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import com.auction.users_service.client.AddressClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+//@Configuration
+public class RestClientConfig {
+
+    //@Value("${addressService.url}")
+    //@Autowired
+    private  final String addressServiceUrl;
+
+    // Constructor for injecting the property
+    public RestClientConfig(@Value("${addressService.url}") String addressServiceUrl) {
+        this.addressServiceUrl = addressServiceUrl;
+    }
+    @Bean
+    public AddressClient addressClient() {
+    RestClient restClient = RestClient.builder()
+            .baseUrl(addressServiceUrl)
+            .build();
+    var restClientAdapter = RestClientAdapter.create(restClient);
+    var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+    return  httpServiceProxyFactory.createClient(AddressClient.class);
+}
+
+}
+
+
